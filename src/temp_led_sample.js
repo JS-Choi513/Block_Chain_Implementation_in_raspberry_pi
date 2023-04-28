2b8c441c5e9ab683d8a00595f7c73c1aad2d46f2
jyan5358 <130760844+jyan5358@users.noreply.github.com>
2023-04-28 11:34:26 +0900

    Update temp_led_sample.js

--- src/temp_led_sample.js
+++ src/temp_led_sample.js
@@ -13,18 +13,70 @@ ledBlue.digitalWrite(0); // Turn BLUE LED off
 //for common anode RGB LED  255 is fully off, and 0 is fully on, so we have to change the value from the client
 
 const sensor = {
-	sensors:[{name: "Indoor", type:22, pin:20} ],
-	read:function(){
-		for (var index in this.sensors){
-			var s = sensorLib.read(this.sensors[index].type, this.sensors[index].pin);
-			console.log(this.sensors[index].name + ": " + s.temperature.toFixed(1)+"°C," + s.humidity.toFixed(1) + "%");
-		ledRed.digitalWrite(1);
-		}
-		ledRed.digitalWrite(0);
-		setTimeout(function(){sensor.read();}, 2500);
+   sensors:[{name: "Indoor", type:22, pin:20} ],
+   read:function(){
+      for (var index in this.sensors){
+         var s = sensorLib.read(this.sensors[index].type, this.sensors[index].pin);
+         console.log(this.sensors[index].name + ": " + s.temperature.toFixed(1)+"°C," + s.humidity.toFixed(1) + "%");
+      ledRed.digitalWrite(1);
+      }
+      ledRed.digitalWrite(0);
+      setTimeout(function(){sensor.read();}, 2500);
 
-	}
+   }
 };
+
+const sensor = {
+  sensors:[{name: "Indoor", type:22, pin:20} ],
+  read:function(){
+    // read sensor data and log it
+    for (var index in this.sensors){
+      var s = sensorLib.read(this.sensors[index].type, this.sensors[index].pin);
+      console.log(this.sensors[index].name + ": " + s.temperature.toFixed(1)+"°C," + s.humidity.toFixed(1) + "%");
+
+      // turn on blue LED when block is being created
+     if (block creation) {
+      ledBlue.digitalWrite(1);
+     }
+
+      // check for network sync and turn on yellow LED
+      if (network_synced) {
+        ledYellow.digitalWrite(1);
+      }
+
+      // check for errors and turn on red LED 
+      if (error_occured) {
+        ledRed.digitalWrite(1);
+      }
+
+      // if everything is working fine, turn on green LED
+      if (!error_occured && network_synced) {
+        ledGreen.digitalWrite(1);
+      }
+    }
+
+    // turn off all LEDs after 2.5 seconds
+    setTimeout(function(){
+      ledRed.digitalWrite(0);
+      ledGreen.digitalWrite(0);
+      ledBlue.digitalWrite(0);
+      sensor.read();
+    }, 2500);
+  }
+};  /jyan change
+
+
 sensor.read();
 
 
+/** project requirement
+ * if(block creation) {
+ * led color = blue
+ * }
+ * else if(network synchronization point) {
+ * led color = yellow}
+ * else if(Fault (Integrity Violation Occurred) {
+ * led color = red})
+ * else if(Normal operation) {
+ * led color = green}
+ */
